339f56593f28fe3d038fdd663601fdeda723a368
thehuy03 <dangthehuh2903@gmail.com>
2023-10-03 23:05:37 +0700

    update login

--- src/main/java/com/Amazing/controller/AccountController.java
+++ src/main/java/com/Amazing/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.Amazing.controller;
 
 import java.util.List;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.Amazing.DAO.UserDAO;
 import com.Amazing.entity.Users;
+import com.Amazing.service.SessionService;
 
 
 
@@ -19,6 +21,8 @@ public class AccountController {
 
 	@Autowired
 	UserDAO account;
+	@Autowired
+	SessionService session;
 	
 	@RequestMapping(method = RequestMethod.GET , value = "AmazingLogin")
 	public String getFormLogin() {
@@ -30,11 +34,23 @@ public class AccountController {
 		Users us = account.findById(user.getUserPhone()).orElse(null);
 		if(us != null) {
 			if(us.getUserPassword().equals(user.getUserPassword())) {
-				System.out.println("Thanh` co^ng");
-			}else {
-				model.addAttribute("loginFail", "hay kiem tra lai tai khoan va mat khau");
-				return "login";
+				session.set("currentUser", us);
+				session.setTimeOut(1*24*60*60);
+				System.out.println("Thanh` co^ng "+ session.toString());
+				return "redirect:/home";
 			}
+		}
+		model.addAttribute("loginFail", "hay kiem tra lai tai khoan va mat khau");
+		return "login";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "logout")
+	public String doLogOut() throws InterruptedException {
+		Users user =  session.get("current_account");
+	
+		if (user != null) {
+			session.setTimeOut(1);
+			Thread.sleep(1000);
 		}
 		return "redirect:/home";
 	}
@@ -50,6 +66,7 @@ public class AccountController {
 		if(!account.existsByUserPhone(us.getUserPhone())) {
 			us.setUserImage("https://i.pinimg.com/564x/80/aa/c3/80aac385ef85310f59dc6c05c92006b8.jpg");
 			System.out.println("Thanh` co^ng dang ki'");
+			account.save(us);
 			return "/user/loginFolder/layoutLogin";
 		}
 		model.addAttribute("registerFail", "hay chac chan ban da dien day du cac form");
